5c709f47465d39766b024f46e58f26add62637e6
김기태 <aqz103@naver.com>
2025-10-17 18:09:37 +0900

    2025/10/17 결제내역 보충

--- src/pages/mypage/Receipt.js
+++ src/pages/mypage/Receipt.js
@@ -1,11 +1,12 @@
+import { useState } from "react";
 import { Alert, Card, Col, Container, Row } from "react-bootstrap";
 
 export default function Receipt() {
 
     const products = [
-        { id: 1, name: "제품 A", period: "2025-10-01 ~ 2025-10-31", images: ["/img1.jpg"] },
-        { id: 2, name: "제품 B", period: "2025-10-05 ~ 2025-11-05", images: ["/img1.jpg"] },
-        { id: 3, name: "제품 C", period: "2025-09-20 ~ 2025-10-20", images: ["/img1.jpg"] }
+        { id: 1, name: "제품 A", period: "2025-10-01 ~ 2025-10-31", images: ["/img1.jpg"], price: 15000 },
+        { id: 2, name: "제품 B", period: "2025-10-05 ~ 2025-11-05", images: ["/img1.jpg"], price: 20000 },
+        { id: 3, name: "제품 C", period: "2025-09-20 ~ 2025-10-20", images: ["/img1.jpg"], price: 3000 }
     ];
 
 
@@ -29,6 +30,12 @@ export default function Receipt() {
                                 <Card.Text>
                                     대여 기간: {bean.period}
                                 </Card.Text>
+                                <div className="d-flex justify-content-end mt-auto">
+                                    <Card.Text className="text-end">
+                                        {/* 여기 내용을 넣으세요 */}
+                                        월 납입금: {bean.price.toLocaleString()}원
+                                    </Card.Text>
+                                </div>
                             </Card>
                         </Col>
                     ))}

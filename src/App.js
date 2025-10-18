1e7eeca0360972eb28bf879376ac105ab9079f71
lcg54 <rjscodla98@naver.com>
2025-10-18 18:22:48 +0900

    상품후기

--- src/App.js
+++ src/App.js
@@ -5,12 +5,35 @@ import Footer from './ui/Footer';
 import { useState } from 'react';
 
 export default function App() {
-  const [user, setUser] = useState(null); // 로그인 로그아웃
+  const ADMIN = {
+    id: 1,
+    username: "admin01",
+    name: "관리자",
+    email: "admin@example.com",
+    password: "admin1234",
+    phone: "010-1111-2222",
+    address: "서울시 강남구 테헤란로 1",
+    profileImage: "admin_profile.jpg",
+    role: "ADMIN",
+    regDate: "2025-10-18"
+  };
+  const USER = {
+      id: 2,
+      username: "user01",
+      name: "홍길동",
+      email: "user@example.com",
+      password: "user1234",
+      phone: "010-3333-4444",
+      address: "서울시 마포구 홍익로 10",
+      profileImage: "user_profile.jpg",
+      role: "USER",
+      regDate: "2025-10-18"
+  }
 
   return (
     <>
       <Header />
-      <AppRoutes user={user} />
+      <AppRoutes user={USER} />
       <Footer />
     </>
   );

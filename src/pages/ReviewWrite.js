581d994a6f3d0fe25fe897f77344c39698c60f2c
seeun <poukelyv@naver.com>
2025-10-21 14:25:13 +0900

    1021

--- src/pages/ReviewWrite.js
+++ src/pages/ReviewWrite.js
@@ -1,6 +1,14 @@
+import { Container, Form } from "react-bootstrap";
+
 export default function App() {
 
   return (
-    <></>
+    <Container>
+      <Form>
+        <Form.Group>
+
+        </Form.Group>
+      </Form>
+    </Container>
   );
 }

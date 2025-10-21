9ec54af70d746bdf6f9e8f3115c3ec3be4cf3b0f
seeun <poukelyv@naver.com>
2025-10-21 10:44:07 +0900

    1021

--- src/pages/InquiryWrite.js
+++ src/pages/InquiryWrite.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { API_BASE_URL } from '../config/config';
+import { API_BASE_URL } from '../config/url';
 import { useNavigate } from 'react-router-dom';
-import { Form, Container, Row, Col } from 'react-bootstrap';
+import { Form, Container, Row, Col, Button } from 'react-bootstrap';
 
 
 
@@ -25,28 +25,33 @@ function App({ }) {
 
     return (
         <Container className="mb-3">
-            <Form.Group as={Row}>
-                <Form.Label column sm={1}>문의 제목</Form.Label>
-                <Col sm={10}><Form.Control placeholder="제목 작성" value={title} maxLength={35} onChange={(e) => setTitle(e.target.value)}></Form.Control></Col>
-            </Form.Group>
-            <Form.Group as={Row}>
-                <Form.Label column sm={1}>문의 사유</Form.Label>
-                <Col sm={10}>
-                    <Form.Select value={reason} onChange={(e) => setReason(e.target.value)}>
-                        <option value="">선택</option>
-                        <option value="배송">배송</option>
-                        <option value="상품">상품</option>
-                        <option value="기타">기타</option>
-                    </Form.Select>
-                </Col>
-            </Form.Group>
-            <Form.Group as={Row}>
-                <Form.Label column sm={1}>문의 내용</Form.Label>
-                <Col sm={10}><Form.Control placeholder="제목 작성" rows={10} as="textarea" maxLength={4000}></Form.Control></Col>
-            </Form.Group>
-            <Form.Group className="d-flex justify-content-end">
-                <Form.Check type="checkbox" label="비공개 체크박스" />
-            </Form.Group>
+            <h2>문의사항</h2>
+            <div>
+                <Form.Group as={Row} >
+                    <Form.Label column sm={1}>문의 제목</Form.Label>
+                    <Col sm={10}><Form.Control placeholder="제목 작성" value={title} maxLength={35} onChange={(e) => setTitle(e.target.value)}></Form.Control></Col>
+                </Form.Group>
+                <Form.Group as={Row}>
+                    <Form.Label column sm={1}>문의 사유</Form.Label>
+                    <Col sm={10}>
+                        <Form.Select value={reason} onChange={(e) => setReason(e.target.value)}>
+                            <option value="">선택</option>
+                            <option value="배송">배송</option>
+                            <option value="상품">상품</option>
+                            <option value="기타">기타</option>
+                        </Form.Select>
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row}>
+                    <Form.Label column sm={1}>문의 내용</Form.Label>
+                    <Col sm={10}><Form.Control placeholder="제목 작성" rows={10} as="textarea" maxLength={4000}></Form.Control></Col>
+                </Form.Group>
+                <Form.Group className="d-flex justify-content-end" >
+                    <Form.Check type="checkbox" label="비공개 체크박스" style={{ position: 'relative', right: 120, flexShrink: 0 }} />
+                </Form.Group>
+                <Button type="submit">제출</Button>
+            </div>
+
         </Container>
     );
 }
